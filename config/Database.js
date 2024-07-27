d402f98b63c0add734782bff16f3e9a09c0b35e1
RayhanPJ <rayhanputra100@gmail.com>
2024-07-27 21:42:45 +0700

    fix database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 const db = new Sequelize('u652011097_tracer_study','u652011097_root','L9i1d0@m0',{
     host: '153.92.15.21',
-    dialect: 'mysql'
-});
+    dialect:'mysql',
+    dialectModule: require('mysql2'),
+  });
 
 export default db;
