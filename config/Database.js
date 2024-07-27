5063d020c7236c82f8603087ea101d0f01282c5e
RayhanPJ <rayhanputra100@gmail.com>
2024-07-27 22:08:25 +0700

    roleback

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,8 @@
-const { Sequelize } = require('sequelize');
-const config = require('./config');
+import { Sequelize } from "sequelize";
+const db = new Sequelize('u652011097_tracer_study','u652011097_root','L9i1d0@m0',{
+    host: '153.92.15.21',
+    dialect:'mysql',
+    dialectModule: require('mysql2'),
+  });
 
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  dialectModule: dbConfig.dialectModule,
-});
-
-module.exports = sequelize;
+export default db;
